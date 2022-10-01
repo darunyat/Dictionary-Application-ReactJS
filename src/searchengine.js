3fc74338a6527d65b046abdea69b7bdec72eacfc
darunyat <108018247+darunyat@users.noreply.github.com>
2022-10-01 19:37:28 +0300

    added Pexels API for images

--- src/searchengine.js
+++ src/searchengine.js
@@ -12,12 +12,20 @@ export default function SearchEngine() {
   function handleResponse(response) {
     setSearchedWord(response.data[0]);
   }
-
+  function handlePexelsResponses(response) {
+    console.log(response.data);
+  }
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${value}`;
     axios.get(apiUrl).then(handleResponse);
+
+    const pexelsAPIKey = `563492ad6f91700001000001a292fde0eca34991abda79a0eb486d98`;
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${value}&per_page=1`;
+    axios
+      .get(pexelsApiUrl, { headers: { Authorization: `${pexelsAPIKey}` } })
+      .then(handlePexelsResponses);
   }
   function updateValue(value) {
     setValue(value.target.value);
